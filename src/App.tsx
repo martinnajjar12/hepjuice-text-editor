eed372758f4591d29671c881acec7c3548d39b4f
Martin Najjar <martin.najjar@hm.com>
2022-12-04 22:34:51 +0100

    Style the editor div

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css';
 import Header from './components/Header';
 import Statistics from './components/Statistics';
+import Editor from './components/Editor';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div className="container">
+      <main className="container">
         <Statistics />
-      </div>
+        <Editor />
+      </main>
     </div>
   );
 }
---
+++ src/components/Editor/components/Editor.tsx
@@ -0,0 +1,7 @@
+import '../Editor.css';
+
+const Editor = () => {
+  return <div className="editor mt-3 p-3"></div>;
+};
+
+export default Editor;
