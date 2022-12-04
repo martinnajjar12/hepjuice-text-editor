b52f6fecd4fae20e138d20dc3b4fa946de806a0a
Martin Najjar <martin.najjar@hm.com>
2022-12-05 00:00:27 +0100

    Add some functionalities to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Statistics from './components/Statistics';
 import Editor from './components/Editor';
 
 function App() {
+  const [cursor, setCursor] = useState('');
+  const [editable, setEditable] = useState(false);
+
+  const handleClick = () => {
+    setCursor('');
+    setEditable(false);
+  };
+
   return (
-    <div className="App">
+    <div className="App" onClick={handleClick}>
       <Header />
       <main className="container">
         <Statistics />
-        <Editor />
+        <Editor
+          editable={editable}
+          setEditable={setEditable}
+          cursor={cursor}
+          setCursor={setCursor}
+        />
       </main>
     </div>
   );
--- src/components/Editor/components/Editor.tsx
+++ src/components/Editor/components/Editor.tsx
@@ -1,7 +1,46 @@
+import React, { useState, useEffect, Dispatch, MouseEvent } from 'react';
 import '../Editor.css';
 
-const Editor = () => {
-  return <div className="editor mt-3 p-3"></div>;
+const Editor = ({
+  editable,
+  setEditable,
+  cursor,
+  setCursor,
+}: {
+  editable: boolean;
+  setEditable: Dispatch<React.SetStateAction<boolean>>;
+  cursor: string;
+  setCursor: Dispatch<React.SetStateAction<string>>;
+}) => {
+  const [content, setContent] = useState('');
+
+  const handleClick = (e: MouseEvent<HTMLElement>) => {
+    e.stopPropagation();
+    setEditable(true);
+    setCursor('|');
+  };
+
+  useEffect(() => {
+    const type = (e: any) => {
+      console.log(e.key);
+      setContent(current => current + e.key);
+    };
+
+    if (editable) {
+      document.addEventListener('keyup', type);
+    }
+
+    return () => {
+      document.removeEventListener('keyup', type);
+    };
+  }, [editable]);
+
+  return (
+    <div className={'editor mt-3 p-3'} onClick={handleClick}>
+      {content}
+      <span className="active">{cursor}</span>
+    </div>
+  );
 };
 
 export default Editor;
